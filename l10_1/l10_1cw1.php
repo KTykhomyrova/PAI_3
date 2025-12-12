a9be89951ebfc8cbc2eb191965c3a22b16622e50
KTykhomyrova <tykhomyrovak@gmail.com>
2025-12-12 14:12:16 +0100

    Add files via upload

--- l10_1/l10_1cw1.php
+++ l10_1/l10_1cw1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,15 +12,30 @@
     $password = "";
     $name = "wazniak";
 
-    $mysqli = mysqli_connect($server, $user, $password, $name);
-    $sqlQuery = "SELECT id_zesp, nazwisko FROM pracownicy WHERE id_zesp = 20 and nazwisko like '%ski' and nazwisko like 'M%'";
+    $mysqli = mysqli_connect($server, $user, $password, $name)
+        or die("Brak połączenia z bazą danych");
+
+    $sqlQuery = "
+        SELECT NAZWISKO, ID_ZESP
+        FROM pracownicy
+        WHERE ID_ZESP = 20 
+        AND (NAZWISKO LIKE 'M%' OR NAZWISKO LIKE '%SKI')
+    ";
+
     $result = mysqli_query($mysqli, $sqlQuery)
         or die("Błąd w zapytaniu");
-    if($result){
-        printf('Result:<br>');
-        
+
+    if ($result) {
+        $rowCount = mysqli_num_rows($result);
+        printf("Znaleziono %d osób:<br>", $rowCount);
+
+        while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+            printf("%s - %s<br>", $row[0], $row[1]);
+        }
     }
 
+    mysqli_close($mysqli);
+    exit(); 
     ?>
 </body>
 </html>
